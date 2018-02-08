8e5c81b2fc1227dc80981d3fd8c588e927d3eb98
Matteo Dessalvi <m.dessalvi@gsi.de>
2018-02-08 22:24:08 +0100

    Adjust line formatting

--- pxe_srv.rb
+++ pxe_srv.rb
@@ -6,19 +6,19 @@ require 'sinatra'
 require 'yaml'
 
 configure do
-    if File.exists?('pxe_srv.yml')
-       # Read the server config from a YAML file:
-       yml_config=YAML.load_file('pxe_srv.yml')
-       # Sets the document root for serving static files:
-       set :public_dir, "#{yml_config['SrvConfig']['public_dir']}"
-       # Sets the 'views' directory for erb templates:
-       set :views, "#{yml_config['SrvConfig']['views']}"
-       # Do not show exceptions
-       set :show_exceptions, false
-    else
-       puts "'pxe_srv.yml' missing in the srv root directory! Exiting..."
-       exit 1
-    end
+  if File.exists?('pxe_srv.yml')
+     # Read the server config from a YAML file:
+     yml_config=YAML.load_file('pxe_srv.yml')
+     # Sets the document root for serving static files:
+     set :public_dir, "#{yml_config['SrvConfig']['public_dir']}"
+     # Sets the 'views' directory for erb templates:
+     set :views, "#{yml_config['SrvConfig']['views']}"
+     # Do not show exceptions
+     set :show_exceptions, false
+  else
+    puts "'pxe_srv.yml' missing in the srv root directory! Exiting..."
+    exit 1
+  end
 end
 
 # Server will always return contents in text format:
