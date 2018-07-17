995b998c9f7930972b791c6acfa4515c81ae2fb4
Victor Penso <vic.penso@gmail.com>
2018-07-17 10:14:35 +0200

    add centos to boot menu

---
+++ source_me.sh
@@ -0,0 +1,25 @@
+# Find the correct path even if dereferenced by a link
+__source=$0
+
+if [[ "$__source" == *bash* ]]; then
+  __source=${BASH_SOURCE[0]}
+fi
+
+__dir="$( dirname $__source )"
+while [ -h $__source ]
+do
+  __source="$( readlink "$__source" )"
+  [[ $__source != /* ]] && __source="$__dir/$__source"
+  __dir="$( cd -P "$( dirname "$__source" )" && pwd )"
+done
+__dir="$( cd -P "$( dirname "$__source" )" && pwd )"
+
+export PXESRV_PATH=$__dir
+
+unset __dir
+unset __source
+
+for file in `\ls $PXESRV_PATH/var/aliases/*.sh`
+do 
+  source $file
+done
